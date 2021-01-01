4a1b8cfa99172d5a2167ae8c2cb7cf43226bc2d3
malcolm's mac mini <38740057+boyesm@users.noreply.github.com>
2021-01-01 17:14:05 -0500

    removed demo files

--- main.py
+++ main.py
@@ -1,28 +1,10 @@
+# Code by Jarvis Coghlin
+# The main function joins all of the useful functions into one coherent place
+
 import sys
-import TwitchIntegration
-import YoutubeIntegration
-import VideoDownloader
-import historyLogger
-
-#Code by Jarvis Coghlin
-#
-#The main function joins all of the useful functions into one coheirent place
-
 
 def main():
-
-    link = TwitchIntegration.getYoutubeLink()
-
-    videoName = YoutubeIntegration.getVideoName(link)
-
-    if VideoDownloader.downloadVideo(link, videoName.strip(" ")):
-        historyLogger.saveGSongLog(link)
-    else:
-        historyLogger.saveBSongLog()
-
-    print("The signal inforamtion is in the file: ", videoName.strip(" "),".txt", sep="")
-
-
+    print("Starting Bertha2 software integration...")
 
 if __name__ == "__main__"():
 
