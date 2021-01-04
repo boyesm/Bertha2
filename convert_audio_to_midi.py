2e41725a428bdb4566ca67f07cb1514e967d8777
malcolm's mac mini <38740057+boyesm@users.noreply.github.com>
2021-01-04 17:24:22 -0500

    started it going

---
+++ convert_audio_to_midi.py
@@ -0,0 +1,2 @@
+def convert_audio_to_midi(audio_file):
+    return midi_file
---
+++ main.py
@@ -0,0 +1,11 @@
+# Code by Jarvis "Smelly" Coghlin
+# The main function joins all of the useful functions into one coherent place
+
+import sys, convert_midi_to_gpio, convert_midi_to_gpio
+
+def main():
+    print("Starting Bertha2 software integration...")
+
+if __name__ == "__main__"():
+
+    main()
