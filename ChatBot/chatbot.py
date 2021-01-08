597701c85a35486b2fc5037f85c6073d9367e56d
BerthaCoghlin <jarviscoghlin@gmail.com>
2021-01-08 01:48:03 -0500

    fkn3fen
    fwm ,ef ewf

---
+++ ChatBot/chatbot.py
@@ -0,0 +1,105 @@
+import sys
+import irc.bot
+import requests
+
+
+# IRC Twitch Integration has forced my hand, and i need a way to get a key from a "fake dictionary".
+# The string that is inputted is scanned through for a certain key and it returns that part of the string+
+def getKeyFromFakeDict(key, parse):
+    dictValue = ""
+    endLocation = 0
+    location = parse.find(key)
+    for i in range(location,len(parse)-location):
+
+        if endLocation == 0:
+            if parse[i] == "!":
+                endLocation = i
+
+    dictValue = parse[location+len(key)+2:endLocation]
+    print("Key found at:", location)
+    print("End key found at:",endLocation)
+
+    return dictValue
+
+
+class TwitchBot(irc.bot.SingleServerIRCBot):
+
+    def __init__(self, username, client_id, token, channel):
+        self.client_id = client_id
+        self.token = token
+        self.channel = '#' + channel
+
+        # Get the channel id, we will need this for v5 API calls
+        url = 'https://api.twitch.tv/kraken/users?login=' + channel
+        headers = {'Client-ID': client_id, 'Accept': 'application/vnd.twitchtv.v5+json'}
+        r = requests.get(url, headers=headers).json()
+        self.channel_id = r['users'][0]['_id']
+
+        # Create IRC bot connection
+        server = 'irc.chat.twitch.tv'
+        port = 6667
+        print('Connecting to ' + server + ' on port ' + str(port) + '...')
+        irc.bot.SingleServerIRCBot.__init__(self, [(server, port, 'oauth:' + token)], username, username)
+
+    def on_welcome(self, c, e):
+        print('Joining ' + self.channel)
+
+        c.cap('REQ', ':twitch.tv/membership')
+        c.cap('REQ', ':twitch.tv/tags')
+        c.cap('REQ', ':twitch.tv/commands')
+        c.join(self.channel)
+
+    def on_pubmsg(self, c, e):
+
+        url = 'https://api.twitch.tv/kraken/channels/' + self.channel_id
+        headers = {'Client-ID': self.client_id, 'Accept': 'application/vnd.twitchtv.v5+json'}
+
+        # If a chat message starts with an exclamation point, try to run it as a command
+        if e.arguments[0][:1] == '!':
+            cmd = e.arguments[0].split(' ')[0][1:]
+            print('Received command: ' + cmd)
+            print("Raw information from message:" + e.arguments[0])
+            print("Raw info from the package:", getKeyFromFakeDict("source",str(e)))
+            self.do_command(e, cmd)
+        return
+
+    def do_command(self, e, cmd):
+        c = self.connection
+
+        # Poll the API to get current game.
+        if cmd == "play":
+
+            c.privmsg(self.channel,"The song has been added to the Queue")
+            
+        # Poll the API the get the current status of the stream
+        elif cmd == "title":
+            url = 'https://api.twitch.tv/kraken/channels/' + self.channel_id
+            headers = {'Client-ID': self.client_id, 'Accept': 'application/vnd.twitchtv.v5+json'}
+            r = requests.get(url, headers=headers).json()
+            c.privmsg(self.channel, r['display_name'] + ' channel title is currently ' + r['status'])
+
+        elif cmd == "toes":
+            url = 'https://api.twitch.tv/kraken/channels/' + self.channel_id
+            headers = {'Client-ID': self.client_id, 'Accept': 'application/vnd.twitchtv.v5+json'}
+            r = requests.get(url, headers=headers).json()
+            c.privmsg(self.channel, 'Yes indeed,' + r['display_name'] + 'has poggers toes! Lots of cheese!')
+        else:
+            c.privmsg(self.channel, "Did not understand command: " + cmd)
+
+
+def main():
+    if len(sys.argv) != 5:
+        print("Usage: twitchbot <username> <client id> <token> <channel>")
+        sys.exit(1)
+
+    username = sys.argv[1]
+    client_id = sys.argv[2]
+    token = sys.argv[3]
+    channel = sys.argv[4]
+
+    bot = TwitchBot(username, client_id, token, channel)
+    bot.start()
+
+
+if __name__ == "__main__":
+    main()
