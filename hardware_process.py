28254a74177e0b00d76b232a4bbb16a834169ba5
malcolm's mac mini <38740057+boyesm@users.noreply.github.com>
2021-05-26 14:53:26 -0400

    in progress

--- hardware_process.py
+++ hardware_process.py
@@ -4,76 +4,82 @@ what this file should do:
 - convert to data playable by the hardware
 - play on hardware
 '''
-import time
+import mido, time, math
 # import asyncio
 
-starting_note = 0 # note that actuators start on
+starting_note = 0 # note that actuators start on (lowest note == 0 == C0)
 number_of_notes = 50 # number of actuators
 time_interval = 20 # milliseconds
 
 # def check_db  ### always be checking db for songs to play
 
 
-def play_midi_file(midi_filename): # add a parameter to determine how much time the file is played for
 
-    # initialize
-        # parse midi file
-        # etc...
-        # signal initization has been completed to calling function (this is so that video can be synced with piano audio) ### toggle flag in db?
+def play_midi_file(midi_filename): # TODO: add a parameter to determine how much time the file is played for
 
+    mid = mido.MidiFile(midi_filename)
 
-
-    # loop
-        # track time in some way
-        # at current time, determine which notes should be depressed, and for how long
-        # call async functions that depress those notes for set duration
-
-    length_of_midi_file # calculate the length in milliseconds of the midi file
-
-    for i in range(0, length_of_midi_file, time_interval):
-
-
-
-
-
-
-
-
-
-
-
-
-
+    for msg in mid.play():
+        print(msg)
+        if msg.type == "note_on":
+            # turn note on with velocity
+            trigger_solenoid(msg.note, msg.time, msg.velocity)
 
 
 #### hardware stuff
 
+def trigger_solenoid(note, duration, velocity): # TODO: make this async
 
+    # check if note is in range of hardware
+    # turn on solenoid pwm output to highest value (this should be a function of velocity)
+    # reduce the pwm signal to hold solenoid in place without overdelivering power to it
 
-### docs: https://circuitpython.readthedocs.io/projects/pca9685/en/latest/examples.html
-from board import SCL, SDA
-import busio
-from adafruit_pca9685 import PCA9685
+    if (note < starting_note) or (note > (starting_note + number_of_notes)): # check if note is in range of hardware
+        return
 
-i2c_bus = busio.I2C(SCL, SDA) # Create the I2C bus interface.
-pca = PCA9685(i2c_bus) # Create a simple PCA9685 class instance.
-pca.frequency = 60 # Set the PWM frequency to 60hz.
-# pca.channels[0].duty_cycle = 0x7FFF # Set the PWM duty cycle for channel zero to 50%. # max value is 65535, min is 0
+    start_time = time.time()
+    time_passed = 0
 
-def power_draw_function(time, note_velocity): # a function to produce an optimal duty cycle value for the solenoid so it doesn't draw unnecessary current
-    starting_duty_cycle # function of velocity
-    final_duty_cycle # function of velocity
+    while time_passed < duration:
+        pwm_value = power_draw_function(time_passed, velocity)
 
-    return duty_cycle_at_time_t
+        # set hardware to pwm value
 
-def turn_on_solenoid(note_number, note_duration, note_velocity): # this needs to be async # note_duration is in milliseconds (or converted to such)
-
-    for i in range(0, note_duration, time_interval): # time values in this function won't be completely accurate
-        pca.channels[note_number].duty_cycle = hex(power_draw_function(i, note_velocity))
-        time.sleep(time_interval/1000)
+        time.sleep(time_interval / 1000)
+        time_passed = time.time() - start_time
 
 
 
+def power_draw_function(time_passed, velocity):
+
+    y = math.log(time_passed) + velocity # this is just an example function, not the final one!
+
+    return y
+
+#
+#
+#
+# ### docs: https://circuitpython.readthedocs.io/projects/pca9685/en/latest/examples.html
+# from board import SCL, SDA
+# import busio
+# from adafruit_pca9685 import PCA9685
+#
+# i2c_bus = busio.I2C(SCL, SDA) # Create the I2C bus interface.
+# pca = PCA9685(i2c_bus) # Create a simple PCA9685 class instance.
+# pca.frequency = 60 # Set the PWM frequency to 60hz.
+# # pca.channels[0].duty_cycle = 0x7FFF # Set the PWM duty cycle for channel zero to 50%. # max value is 65535, min is 0
+#
+# def power_draw_function(time, note_velocity): # a function to produce an optimal duty cycle value for the solenoid so it doesn't draw unnecessary current
+#     starting_duty_cycle # function of velocity
+#     final_duty_cycle # function of velocity
+#
+#     return duty_cycle_at_time_t
+#
+# def turn_on_solenoid(note_number, note_duration, note_velocity): # this needs to be async # note_duration is in milliseconds (or converted to such)
+#
+#     for i in range(0, note_duration, time_interval): # time values in this function won't be completely accurate
+#         pca.channels[note_number].duty_cycle = hex(power_draw_function(i, note_velocity))
+#         time.sleep(time_interval/1000)
 
 
 
