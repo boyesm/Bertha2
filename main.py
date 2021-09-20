4d2b62b84947aeffaa00b699d0c5e2e9a0e12e10
Malcolm Boyes <38740057+boyesm@users.noreply.github.com>
2021-09-19 22:19:10 -0400

    played with some shit

---
+++ main.py
@@ -0,0 +1,21 @@
+
+from twitch_chat import check_for_commands
+
+
+videos = []
+
+# poll twitch for new videos OR select a video from the command line
+# when a video is returned, add it to a global variable that everyone can access
+
+
+
+# convert videos
+
+# play video
+
+async def main():
+
+    await check_for_commands()  # returns one message
+    # if message is a play command, queue video
+
+
