f19794b2eeecf56ab885033a7edf94535e1f8878
Malcolm Boyes <38740057+boyesm@users.noreply.github.com>
2021-12-16 22:24:09 -0500

    cleaned up arduino_code.ino

--- arduino_code/arduino_code.ino
+++ arduino_code/arduino_code.ino
@@ -19,82 +19,35 @@ int ab;
 
 void setup() {
 
- Serial.begin(serial_baudrate);
- Serial.setTimeout(1);
+  Serial.begin(serial_baudrate);
+  Serial.setTimeout(1);
   Wire.begin();
-
+  
   pwmController.resetDevices();       // Resets all PCA9685 devices on i2c line
-
-    pwmController.init();               // Initializes module using default totem-pole driver mode, and default phase balancer
-
-    pwmController.setPWMFrequency(500); // Set PWM freq to 500Hz (default is 200Hz, supports 24Hz to 1526Hz)
- 
-
-
-
- while (!Serial) {
+  
+  pwmController.init();               // Initializes module using default totem-pole driver mode, and default phase balancer
+  
+  pwmController.setPWMFrequency(500); // Set PWM freq to 500Hz (default is 200Hz, supports 24Hz to 1526Hz)
+  
+  while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB
- }
-
-  // pwmController.setChannelPWM(0, 128 << 4);
+  }
 }
 
 
 
 void loop() {
 
-//  if (Serial.available() > 0) {
-//     ab = Serial.readString().toInt();
-//    ab = Serial.parseInt();
-//     Serial.print(ab);
-//    Serial.print("test!");
-//  }
-
-// /*
-
-// append parse int to array
-// when array is 2 long proceed
-
-if (Serial.available() > 0) {
-
-//  long int i = Serial.parseInt();
-
-  Serial.readBytes(buff, 2);
-
-  Serial.print(buff[0]);
-  Serial.print(" ");
-  Serial.println(buff[1]);
+  if (Serial.available() > 0) {
   
-
-//  Serial.println(i);
-
-//  if(x == 0){
-//    buff[0] = i;
-//    x=1;
-//  } else if (x==1){
-//    buff[1] = i;
-//    x=0;
-//
-//    Serial.print(buff[0]);
-//    Serial.print(" ");
-//    Serial.println(buff[1]);
-//
-//    // no need to clear buffer
-//
-////    pwm1.setPWM(buff[0], buff[1], 0);
-//
-    pwmController.setChannelPWM(buff[0], buff[1] << 4);
-//  }
-//  
-
-  
-}
-
-//*/
+    Serial.readBytes(buff, 2);
     
+    Serial.print(buff[0]);
+    Serial.print(" ");
+    Serial.println(buff[1]);
+    
+    
+    pwmController.setChannelPWM(buff[0], buff[1] << 4);
   
-
-  
-
-
+  }
 }
